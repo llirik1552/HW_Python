424d0931b1179f91b7d4c836b1b6d206d8fd938f
llirik1552 <llirik1552@gmail.com>
2025-03-16 21:03:14 +0300

    Update lesson_1_task_5.py

--- lesson_1_task_5.py
+++ lesson_1_task_5.py
@@ -1,4 +1,13 @@
 def print_letter(let):
     print(let, end='')
 
-print_letter(88005553535)
+for _ in range(11):
+    print_letter('8')
+    print_letter('8')
+    print_letter('0')
+    print_letter('0')
+    print_letter('5')
+    print_letter('5')
+    print_letter('5')
+    print_letter('3')
+    print_letter('5')
